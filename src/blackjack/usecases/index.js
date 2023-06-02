e3efc487ba6e9d14d3fac176fbe58180f127a79b
Agustin Decuadro <agustin.dec@icloud.com>
2023-06-02 17:26:45 -0300

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './create-deck';
+export { pedirCarta } from './request-card';
+export { valorCarta } from './card-value';
+export { turnoComputadora } from './computer-turn';
+export { crearCartaHTML } from './create-card-html';
